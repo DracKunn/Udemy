2414d44ecf3d24784dbf1d6c5598cd22a01e14f0
Brecht Van Rie <83006023+DracKunn@users.noreply.github.com>
2022-01-19 15:43:12 +0100

    Update fundamentals2script.js
    added all lesson notes on pcB. Will try to keep all notes updated here so I always have full notes when switching between machines.

--- fundamentals2script.js
+++ fundamentals2script.js
@@ -1,3 +1,96 @@
+/* 'use strict'; //strictmode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+//const private = true;
+//const if = 23;
+
+
+
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+
+
+
+
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+// Arrow Function
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+console.log(yearsUntilRetirement(1988, 'B-Rex'));
+
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+*/
+////////////////////
+
+// Coding challenge #1
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -20,6 +113,7 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 
 GOOD LUCK 😀
 */
+/*
 // 1.
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
 
@@ -58,3 +152,44 @@ function checkWinner(avgDolphins, avgKoalas) {
         console.log('No team wins!');
     }
 }
+*/
+// Introduction to arrays
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const yearsExample = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// arrays, even when using const are still mutable, so you can change a vlaue. however we can't chage the whole array. friends = ['Bob', 'Alice']; would give an error because we created the friends array using const.
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+//calcAge(years); does'nt work because the function expects an expression and not an array.
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
